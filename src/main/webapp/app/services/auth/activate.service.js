da6d7d3fd74aa95cd507182d732cb52307d4ce3e
karthik <aleti.lohith@gmail.com>
2016-12-31 15:47:03 +0530

    initial commit

---
+++ src/main/webapp/app/services/auth/activate.service.js
@@ -0,0 +1,17 @@
+(function() {
+    'use strict';
+
+    angular
+        .module('jhipsterDemoApp')
+        .factory('Activate', Activate);
+
+    Activate.$inject = ['$resource'];
+
+    function Activate ($resource) {
+        var service = $resource('api/activate', {}, {
+            'get': { method: 'GET', params: {}, isArray: false}
+        });
+
+        return service;
+    }
+})();
